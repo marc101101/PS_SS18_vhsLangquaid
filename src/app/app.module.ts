75dfe1c16022b86f893536c4a91d3453f9981cb1
Markus Guder <progamer101101@gmx.de>
2018-07-03 19:00:09 +0200

    Init Project Structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+
+import {AppComponent} from './app.component';
+import {LessonsComponent} from './lessons/lessons.component';
+
+import {LessonsService} from "./services/lessons.service";
+import {ReactiveFormsModule} from "@angular/forms";
+
+import {environment} from '../environments/environment';
+import {ServiceWorkerModule} from '@angular/service-worker';
+
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/shareReplay';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/first';
+import 'rxjs/add/observable/of';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AppRoutingModule} from "./app-routing.module";
+import {NewsletterService} from "./services/newsletter.service";
+
+
+
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        LessonsComponent
+    ],
+    imports: [
+        BrowserModule.withServerTransition({ appId: 'serverApp' }),
+        HttpClientModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+
+    ],
+    providers: [
+        LessonsService,
+        NewsletterService
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
