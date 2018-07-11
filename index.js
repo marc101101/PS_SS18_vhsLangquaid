740bc90a1176ee5575548057eec1a63428316c2f
uids4029 <markus.2.guder@continental-corporation.com>
2018-07-11 13:50:20 +0200

    Fixed starting statements #40

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 'use strict';
 
-var fs = require('fs'),
+const fs = require('fs'),
     path = require('path'),
     http = require('http');
 
-var app = require('connect')();
-var swaggerTools = require('swagger-tools');
-var jsyaml = require('js-yaml');
-var serverPort = 8080;
+const express = require('express');
+const cors = require('cors');
+
+const swaggerTools = require('swagger-tools');
+const jsyaml = require('js-yaml');
+const colors = require('colors');
+
+const serverPort = 8080;
+const app = express();
 
 // swaggerRouter configuration
-var options = {
-  swaggerUi: path.join(__dirname, '/swagger.json'),
-  controllers: path.join(__dirname, './controllers'),
+const options = {
+  swaggerUi: path.join(__dirname, '/server/swagger.json'),
+  controllers: path.join(__dirname, './server/controllers'),
   useStubs: process.env.NODE_ENV === 'development' // Conditionally turn on stubs (mock mode)
 };
 
+//Frontend provided by backend service
+app.use('/', express.static(path.join(__dirname, 'dist')));
+
+app.use(cors(getCorsOptions()));
+
 // The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
-var spec = fs.readFileSync(path.join(__dirname,'server/api/swagger.yaml'), 'utf8');
-var swaggerDoc = jsyaml.safeLoad(spec);
+const spec = fs.readFileSync(path.join(__dirname,'server/api/swagger.yaml'), 'utf8');
+
+const swaggerDoc = jsyaml.safeLoad(spec);
 
 // Initialize the Swagger middleware
 swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
@@ -37,8 +48,35 @@ swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
 
   // Start the server
   http.createServer(app).listen(serverPort, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+    console.log('[REST] '.green + 'Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+    console.log('[REST] '.green + 'Swagger-ui is available on http://localhost:%d/docs', serverPort);
+    console.log('[FRONTEND] '.green + 'UI is available on http://localhost:%d/', serverPort);
   });
 
 });
+
+function getCorsOptions() {
+
+  let corsAccessControlAllowOrigin = "*";
+  let corsAccessControlAllowHeaders = "Authorization, Content-Type, Content-Length, Content-Disposition";
+  let corsAccessControlAllowMethods = "GET, PUT, POST, DELETE, HEAD, OPTIONS";
+
+  if (typeof process.env.corsAccessControlAllowOrigin !== 'undefined') {
+    corsAccessControlAllowOrigin = process.env.corsAccessControlAllowOrigin;
+  }
+
+  if (typeof process.env.corsAccessControlAllowHeaders !== 'undefined') {
+    corsAccessControlAllowHeaders = process.env.corsAccessControlAllowHeaders;
+  }
+
+  if (typeof process.env.corsAccessControlAllowMethods !== 'undefined') {
+    corsAccessControlAllowMethods = process.env.corsAccessControlAllowMethods;
+  }
+
+  return {
+    origin: corsAccessControlAllowOrigin,
+    methods: corsAccessControlAllowMethods,
+    allowedHeaders: corsAccessControlAllowHeaders,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  };
+}
