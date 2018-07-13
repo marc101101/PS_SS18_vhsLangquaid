b99dd1e3d4601e2585d898815e46f862ecf2c9fe
Markus Guder <progamer101101@gmx.de>
2018-07-13 22:51:54 +0200

    added jwt generator #9

--- server/service/AuthService.js
+++ server/service/AuthService.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var VerifyToken = require('../utils/VerifyToken');
+var User = require('../utils/database').User;
+var knex = require('../utils/database').knex;
 
 /**
  * Configure JWT
@@ -17,9 +19,9 @@ var config = require('../config'); // get config file
  **/
 exports.authPOST = function (data) {
  
-  return new Promise(function (resolve, reject) {
+  /*return new Promise(function (resolve, reject) {
     
-    User.findOne({ email: data.email }, function (err, user) {
+    User.where({ email: data.email }, function (err, user) {
       if (err) return res.status(500).send('Error on the server.');
       if (!user) return res.status(404).send('No user found.');
       
@@ -36,6 +38,32 @@ exports.authPOST = function (data) {
       // return the information including token as JSON
       res.status(200).send({ auth: true, token: token });
     });
+    });*/
 
-  });
+    return new Promise((resolve, reject) => {
+
+      User.where({ TEIL_EMAIL: data.email })
+        .fetch().then((user) => {
+          /*if (err) reject(res.status(500).send('Error on the server.'));
+          if (!user) reject(res.status(404).send('No user found.'));
+          
+          // check if the password is valid
+          /*var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+          if (!passwordIsValid) reject(res.status(401).send({ auth: false, token: null }));*/
+      
+          // if user is found and password is valid
+          // create a token
+          var token = jwt.sign({ id: user.attributes.TEIL_ID }, config.secret, {
+            expiresIn: 86400 // expires in 24 hours
+          });
+      
+          // return the information including token as JSON
+          reject(res.status(200).send({ auth: true, token: token }));
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+
+  
 }
