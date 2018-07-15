c3572535662bd24b928b26f482eb29bde8b575b5
Markus Guder <progamer101101@gmx.de>
2018-07-15 20:52:34 +0200

    add auth test scenarios

---
+++ server/test/auth.js
@@ -0,0 +1,40 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../../index');
+
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+let validUser = {email: 'hans@vhslq.de', password: 'hunter22'};
+let invalidUser = {email: 'franzXYZ@vhslq.de', password: 'hunter22'}
+
+describe('Authentication', () => {
+    /*before(() => {
+      return userService
+        .clearDataBase()
+    });*/
+  
+  it("it should return an valid jwt token", (done) => {
+    chai.request(server)
+      .post('/v1/auth')
+      .send(validUser)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.should.contain.keys('token');
+        done();
+      })
+  });
+
+  it("it should return status 401 (not authorized)", (done) => {
+    chai.request(server)
+      .post('/v1/auth')
+      .send(invalidUser)
+      .end((err, res) => {
+        res.should.have.status(401);
+        done();
+      })
+  });
+
+});
