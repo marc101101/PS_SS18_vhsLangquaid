a36601555c2721e8ac4285b15ca3e4070be769a4
Hans-Martin Schuller <hsch@num42.de>
2018-07-20 13:17:52 +0200

    add table definitions for Course

--- server/utils/database.js
+++ server/utils/database.js
@@ -10,18 +10,35 @@ var knex = require('knex')({
   }
 });
 var bookshelf = require('bookshelf')(knex);
+exports.knex = knex;
+
+
+let course = bookshelf.Model.extend({
+  tableName: "vhslq_kurse",
+  // use function() instead of lambda big arrow here otherwise "this" is not the correct this ....
+  category: function() {
+    return this.belongsTo(category, "RUB_ID")
+  }
+})
+
+let category  = bookshelf.Model.extend({
+  tableName: 'vhslq_rubriken',
+  // use function() instead of lambda big arrow here otherwise "this" is not the correct this ....
+  courses: function() {
+    return this.hasMany(course, "KURS_RUB_ID", "RUB_ID")
+  }
+})
 
 exports.User = bookshelf.Model.extend({
   tableName: 'vhslq_teilnehmer',
 });
 
-exports.knex = knex;
-
-let category  = bookshelf.Model.extend({
-  tableName: 'vhslq_rubriken',
-})
 exports.Category = category
-
 exports.Categories = bookshelf.Collection.extend({
   model: category
+})
+
+exports.Course = course
+exports.Courses = bookshelf.Collection.extend({
+  model: course
 })
