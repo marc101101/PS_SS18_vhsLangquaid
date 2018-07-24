035e7fc477bff5b9d2918f078f384c53d004cf77
Hans-Martin Schuller <hsch@num42.de>
2018-07-24 10:13:30 +0200

    introduce error handling

---
+++ server/utils/errors.js
@@ -0,0 +1,46 @@
+const errors = Object.freeze({
+  invalidAuth: () => {
+    return {
+    code: 401,
+    message: "Invalid authentication credentials."
+    }
+  },
+  missingAuth: () => {
+    return {
+    code: 401,
+    message: "No authtoken provided."
+    }
+  },
+  forbidden: (permission, file) => {
+    return {
+      code: 403,
+      message: "Permission "+permission+"denied on file "+file+"."
+    }
+  },
+  notFound: (permission, file) => {
+    return {
+    code: 404,
+    message: "Permission "+permission+"denied on file "+file+"."
+    }
+  },
+  conflict: (ressource) => {
+    return {
+    code: 409,
+    message: "Resource "+ressource+"already exists."
+    }
+  },
+  forbidden: () => {
+    return {
+    code: 403,
+    message: "Permission 'xxx' denied on file 'yyy'."
+    }
+  },
+  server: () => {
+    return {
+    code: 500,
+    message: "Internal Server Error."
+    }
+  }
+});
+
+module.exports = errors;
