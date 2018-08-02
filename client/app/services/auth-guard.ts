10a3657f6190d0f5f5f5417cb5d78fc6f4ce14df
Markus Guder <progamer101101@gmx.de>
2018-08-02 20:57:45 +0200

    Added content to components

--- client/app/services/auth-guard.ts
+++ client/app/services/auth-guard.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthGuard {
--- client/app/services/auth.service.ts
+++ client/app/services/auth.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '../../../node_modules/@angular/common/http';
+import { Observable } from '../../../node_modules/rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,15 +8,16 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   private authStatus: boolean = false;
+  private url: string;
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
 
   isLoggedIn(): boolean{
     return this.authStatus;
   }
 
-  login(): void{
-
+  login(email: string, password: string): Observable<any>{
+    return this.http.post(this.url + "/auth", {email: email, password: password})
   }
 
   logout(): void{
@@ -22,6 +25,6 @@ export class AuthService {
   }
 
   register(): void{
-    
+
   }
 }
