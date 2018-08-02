a33285d9e7c8901fdbad625306d60305f61b1a20
Markus Guder <progamer101101@gmx.de>
2018-08-02 21:10:37 +0200

    Add router import

--- client/app/services/auth.service.ts
+++ client/app/services/auth.service.ts
@@ -17,7 +17,7 @@ export class AuthService {
   }
 
   login(email: string, password: string): Observable<any>{
-    return this.http.post(this.url + "/auth", {email: email, password: password})
+    return this.http.post(this.url + "/auth", {email: email, password: password});
   }
 
   logout(): void{
