5e97a3b9935d07144c1204487ff847508b7d2827
Markus Guder <progamer101101@gmx.de>
2018-08-16 17:50:20 +0200

    Updated authentification system

--- client/app/components/login/login.component.ts
+++ client/app/components/login/login.component.ts
@@ -5,6 +5,11 @@ import {
 
 import { AuthService } from '../../services/auth.service';
 import { UserData } from '../../model/UserData';
+import { JwtHelper } from 'angular2-jwt';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AlertService } from '../../services/alert.service';
+import { log } from 'util';
+
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
@@ -13,15 +18,35 @@ import { UserData } from '../../model/UserData';
 export class LoginComponent implements OnInit {
 
   public model: UserData = new UserData("", "");  
+  private jwtHelper: JwtHelper = new JwtHelper();
+  private returnUrl: string;
 
-  constructor(private authSerive: AuthService) {}
+  constructor(private authSerive: AuthService, private router:Router, private alertService: AlertService, private route: ActivatedRoute) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/categories';
+  }
 
   onSubmit() {
-    console.log("SUBMIT");
-    this.authSerive.login(this.model).subscribe(response => {
-      console.log(response);
+    this.authSerive.login(this.model).subscribe(data => {
+      // login successful if there's a jwt token in the response
+      if (data.token && !this.jwtHelper.isTokenExpired(data.token)) {
+        // store jwt token in local storage to keep user logged in between page refreshes
+        localStorage.setItem('token', data.token);
+        console.log(this.returnUrl);
+        
+        this.router.navigate(['/categories']).catch(error => {
+          console.log(error);
+        });
+      } else {       
+        //this.alertService.validateResponse({'status': 401}, true);
+      }
+    },
+    error => {
+      console.log(error);
+      
+      //this.alertService.validateResponse(error, true);
+      //this.loading = false;
     });
   }
 
--- client/app/guards/auth.guard.ts
+++ client/app/guards/auth.guard.ts
@@ -1,16 +1,46 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { JwtHelper } from 'angular2-jwt';
+import { isUndefined, log } from 'util';
 
 @Injectable()
 export class AuthGuard {
 
+  private user: string;
+  private exp: string;
+  private jwtHelper: JwtHelper = new JwtHelper();
+
   constructor( private authService : AuthService, private router : Router ) {
+    this.parseUserDetailsFromToken();
   }
 
   canActivate( route : ActivatedRouteSnapshot, state : RouterStateSnapshot ) {
-    if(this.authService.isLoggedIn()) return true;
-    // else navigate to login
+    if(!sessionStorage.getItem('user')) {
+      this.parseUserDetailsFromToken();
+    }
+    
+    if(!isUndefined(this.user)){
+      return true;
+    }
+
+    else{
+      this.router.navigate(['/login']).catch(error => {
+        console.log(error);        
+      });
+      return false;
+    }
+  }
+
+  parseUserDetailsFromToken() {
+    if (localStorage.getItem('access_token')){
+      const decodedToken = this.jwtHelper.decodeToken(localStorage.getItem('token'));
+      console.log(decodedToken);
+      
+      this.user = decodedToken.id;
+      this.exp = decodedToken.exp;
+      sessionStorage.setItem('user', this.user);
+    }
   }
 
 }
