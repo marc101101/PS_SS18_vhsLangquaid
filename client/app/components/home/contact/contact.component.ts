c5ded0b8cbab898173a48153c2ebb78eaaf6a527
Markus Guder <progamer101101@gmx.de>
2018-09-16 22:44:40 +0200

    Finished contact form

--- client/app/components/home/contact/contact.component.ts
+++ client/app/components/home/contact/contact.component.ts
@@ -1,25 +1,33 @@
 import { Component, OnInit, ViewChild, Renderer2, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
+import { ContactService } from '../shared/contact.service';
 
 @Component({
   selector: 'contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
   public dataIsAvailable: boolean = false;
-  public singleCourse: any;
-  public button_text: string = "Anmelden";
-  public showButton: boolean = false;
-  public alreadyApplied: boolean = false;
+  public button_text: string = "Feedback senden"
+  public contactMessage: any = {
+    text: "",
+    bewertung: null,
+  };
 
-  @ViewChild('button') elButton: ElementRef;
+  @ViewChild('send') sendButton: ElementRef;
 
   constructor(
     private router: Router, 
-    private renderer: Renderer2) { }
+    private renderer: Renderer2,
+    private contactService: ContactService) { }
 
-  ngOnInit() {
-    
+  submit():void{
+    this.contactService.postContactFeedback(this.contactMessage).subscribe(response => {
+      if(response.name != "HttpResponseError"){
+        this.renderer.addClass(this.sendButton.nativeElement, 'is-primary-save');
+        this.button_text = 'Senden erfolgreich';
+      }  
+    });
   }
 }
