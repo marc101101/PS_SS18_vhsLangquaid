b6a5eac553a99dd3266ed0957614b02b85c8ddeb
Hans Schuller <hsch@num42.de>
2018-09-16 19:17:58 +0200

    trimmer

--- server/utils/trim.js
+++ server/utils/trim.js
@@ -1,7 +1,16 @@
 
 let courseFields = ["KURS_ID", "KURS_NAME", "KURS_BESCHREIBUNG", "location", "teacher"];
-let userFields = ["TEIL_ID", "TEIL_EMAIL"];
+let categoryFields = ["RUB_ID", "RUB_NAME", "RUB_TITEL", "RUB_TEXT"];
+let userFields = ["TEIL_ID", "TEIL_EMAIL", "TEIL_VORNAME", "TEIL_NACHNAME"];
 let locationFields = ["ORT_ID", "ORT_NAME", "ORT_STRASSE", "ORT_PLZ", "ORT_ORTSNAME"];
+
+// for testing purposes also add field DATENHISTORY to each object 
+if (process.env.NODE_ENV === 'test') {
+    courseFields.push("DATENHISTORY");
+    categoryFields.push("DATENHISTORY");
+    userFields.push("DATENHISTORY");
+    locationFields.push("DATENHISTORY");
+}
 
 let trimObject = (object, array) => {
     return array
@@ -21,6 +30,16 @@ let trimCourse = (course) => {
 let trimCourses = (courses) => {
     return courses.map(course => trimCourse(course));
 }
+let trimCategory = (category) => {
+    let newCategory = trimObject(category, categoryFields);
+    newCategory.courses = newCategory.courses ? trimCourses(newCategory.courses) : {};
+    return newCategory;
+}
+
+let trimCategories = (categories) => { 
+    return categories.map(category => trimCategory(category));
+}
+
 let trimUser = (user) => {
     return trimObject(user, userFields);
 }
@@ -31,9 +50,11 @@ let trimLocation = (location) => {
     return trimObject(location, locationFields);
 }
 
-
 exports.course = trimCourse;
 exports.courses = trimCourses;
+
+exports.category = trimCategory;
+exports.categories = trimCategories;
 
 exports.user = trimUser;
 exports.users = trimUsers;
