59609c2fa99c36beafc5fe77d0160a73f7e3286e
marc101101 <progamer101101@gmx.de>
2018-09-26 13:38:58 +0200

    Updated routing

--- client/app/components/login/login.component.ts
+++ client/app/components/login/login.component.ts
@@ -7,23 +7,31 @@ import { AuthService } from '../../services/auth.service';
 import { UserData } from '../../models/UserData';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { Router } from '@angular/router';
+import { log } from 'util';
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   public model: UserData = new UserData("", "");  
   private jwtHelper: JwtHelperService = new JwtHelperService();
 
   constructor(private authSerive: AuthService, private router:Router) {}
 
+  ngOnInit() {
+    this.authSerive.isLoggedIn().subscribe(response => {
+      if(response){
+        this.router.navigate(['/home/kategorien']);
+      }
+    });
+  }
+
   onSubmit() {
     this.authSerive.login(this.model).subscribe(response => {
       // login successful if there's a jwt token in the response   
-      console.log("erer", response);   
       if (response.token && !this.jwtHelper.isTokenExpired(response.token)) {
         // store jwt token in local storage to keep user logged in between page refreshes
         
